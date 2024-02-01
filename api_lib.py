020d837d02d19c30812a7249430bf4bbd04069f5
savelov <savelovG13b@hotmail.com>
2024-02-01 18:24:08 +0500

    Task №1

---
+++ api_lib.py
@@ -0,0 +1,64 @@
+import requests
+import math
+
+
+def get_static(**params):
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=params)
+    return response.content
+
+
+def geocode(**params):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    response = requests.get(geocoder_api_server, params=params)
+
+    if not response:
+        print('Error', response.reason, response.status_code)
+        exit(-1)
+
+    return response.json()
+
+
+def get_toponym(json_data):
+    return json_data["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+
+
+def get_toponym_coord(toponym):
+    return list(map(float, toponym['Point']['pos'].split(" ")))
+
+
+def get_spn(toponym):
+    lon1, lat1 = map(float, toponym['boundedBy']['Envelope']["lowerCorner"].split())
+    lon2, lat2 = map(float, toponym['boundedBy']['Envelope']["upperCorner"].split())
+    return lon2 - lon1, lat2 - lat1
+
+
+def search_org(**params):
+    search_api = 'https://search-maps.yandex.ru/v1/'
+    api_key = 'dda3ddba-c9ea-4ead-9010-f43fbc15c6e3'
+    params['apikey'] = api_key
+    response = requests.get(search_api, params=params)
+
+    if not response:
+        print('Error', response.reason, response.status_code)
+        exit(-1)
+
+    return response.json()
+
+
+def get_org(json_data):
+    org = json_data['features']
+    return org
+
+
+def get_org_info(org):
+    org_name = org["properties"]["CompanyMetaData"]["name"]
+    org_address = org["properties"]["CompanyMetaData"]["address"]
+    org_hours = org['properties']['CompanyMetaData']['Hours']
+    return org_name, org_address, org_hours['text']
+
+
+def get_org_coords(org):
+    point = org['geometry']['coordinates']
+    return point
---
+++ main.py
@@ -0,0 +1,64 @@
+import os
+import sys
+from io import BytesIO
+import pygame
+from api_lib import get_static
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class BigMap:
+    def __init__(self):
+        self.image = None
+        self.ll = 60.153191, 55.156353
+        self.layer = 'map'
+        self.z = 17
+        self.update_map()
+
+    def update_map(self):
+        map_params = {
+            "ll": ",".join(map(str, self.ll)),
+            'z': self.z,
+            "l": "map",
+            'size': '650,450'
+        }
+        image = BytesIO(get_static(**map_params))
+        self.image = pygame.image.load(image)
+
+    def event_handler(self, event):
+        pass
+
+    def draw(self, surf):
+        surf.blit(self.image, (0, 0))
+
+
+if __name__ == '__main__':
+    pygame.init()
+    SIZE = W, H = 650, 450
+    screen = pygame.display.set_mode(SIZE)
+    app = BigMap()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            app.event_handler(event)
+
+        screen.fill('black')
+        app.draw(screen)
+        pygame.display.flip()
